11c5f7098ff6ec2a480ea523302becf9ee6a6eb8
tutaCattaneo <49413365+tutaCattaneo@users.noreply.github.com>
2022-09-29 10:13:51 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Be-Tech
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-1d3oam)
---
+++ index.js
@@ -0,0 +1,17 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+let juegos = ['Futbol','Rugby','Doom','League'];
+let primero=juegos[2-2];
+let ultimo=juegos[juegos.length -1];
+
+
+console.log({primero,ultimo});
+
+
+
+
